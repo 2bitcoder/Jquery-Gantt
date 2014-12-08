7e9f6d9aaaca44b45713b9f5e1cc5e94909508a3
lavrton <lavrton@gmail.com>
2014-12-08 09:03:09 +0700

    add form

---
+++ src/js/views/addFormView.js
@@ -0,0 +1,144 @@
+/**
+ * Created by lavrton on 02.12.2014.
+ */
+var fs = require('fs');
+var template = fs.readFileSync(__dirname + '/addForm.html', 'utf8');
+
+var demoResources = [{"wbsid":1,"res_id":1,"res_name":"Joe Black","res_allocation":60},{"wbsid":3,"res_id":2,"res_name":"John Blackmore","res_allocation":40}];
+
+var AddFormView = Backbone.View.extend({
+    el: document.body,
+    template: template,
+    events: {
+        'click .new-task': 'openForm',
+        'click #submitForm': 'submitForm'
+    },
+    initialize: function () {
+        this.$el.append(this.template);
+        this._prepareForm();
+        this._initResources();
+        this.listenTo(this.collection, 'add', this._setupParentSelector);
+    },
+    _initResources: function () {
+        // Resources from backend
+        var $resources = '<select id="resources"  name="resources[]" multiple="multiple">';
+        demoResources.forEach(function (resource) {
+            $resources += '<option value="' + resource.res_id + '">' + resource.res_name + '</option>';
+        });
+        $resources += '</select>';
+        // add backend to the task list
+        $('.resources').append($resources);
+
+        // initialize multiple selectors
+        $('#resources').chosen({width: '95%'});
+    },
+    _formValidationParams : {
+        name: {
+            identifier: 'name',
+            rules: [
+                {
+                    type: 'empty',
+                    prompt: 'Please enter a task name'
+                }
+            ]
+        },
+        complete: {
+            identifier: 'complete',
+            rules: [
+                {
+                    type: 'empty',
+                    prompt: 'Please enter an estimate days'
+                }
+            ]
+        },
+        start: {
+            identifier: 'start',
+            rules: [
+                {
+                    type: 'empty',
+                    prompt: 'Please set a start date'
+                }
+            ]
+        },
+        end: {
+            identifier: 'end',
+            rules: [
+                {
+                    type: 'empty',
+                    prompt: 'Please set an end date'
+                }
+            ]
+        },
+        duration: {
+            identifier: 'duration',
+            rules: [
+                {
+                    type: 'empty',
+                    prompt: 'Please set a valid duration'
+                }
+            ]
+        },
+        status: {
+            identifier: 'status',
+            rules: [
+                {
+                    type: 'empty',
+                    prompt: 'Please select a status'
+                }
+            ]
+        }
+    },
+    _prepareForm : function() {
+        $('.masthead .information').transition('scale in');
+        $('.ui.form').form(this._formValidationParams);
+        // assign random parent color
+        $('input[name="color"]').val('#'+Math.floor(Math.random()*16777215).toString(16));
+    },
+    _setupParentSelector : function() {
+        var $selector = $('[name="parentid"]');
+        $selector.empty();
+        $selector.append('<option value="0">Main Project</option>');
+        this.collection.each(function(task) {
+            var parentId = parseInt(task.get('parentid'), 10);
+            if(parentId === 0){
+                $selector.append('<option value="' + task.id + '">' + task.get('name') + '</option>');
+            }
+        });
+        $('select.dropdown').dropdown();
+    },
+    render : function() {
+        "use strict";
+        // initialize dropdown
+        this._setupParentSelector();
+    },
+    openForm: function() {
+        $('.ui.add-new-task').modal('setting', 'transition', 'vertical flip').modal('show');
+    },
+    submitForm: function(e) {
+        var form = $("#new-task-form");
+
+        var data = {};
+        $(form).serializeArray().forEach(function(input) {
+            data[input.name] = input.value;
+        });
+
+        var sortindex = 0;
+        var ref_model = this.collection.get(data.reference_id);
+        if (ref_model) {
+            var insertPos = data.insertPos;
+            sortindex = ref_model.get('sortindex') + (insertPos === 'above' ? -0.5 : 0.5);
+        } else {
+            sortindex = (this.collection.last().get('sortindex') + 1);
+        }
+        data.sortindex = sortindex;
+
+        if (form.get(0).checkValidity()) {
+            e.preventDefault();
+            var task = this.collection.add(data, {parse : true});
+            task.save();
+            $('.ui.modal').modal('hide');
+        }
+    }
+});
+
+module.exports = AddFormView;
