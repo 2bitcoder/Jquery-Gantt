4b3bb1761bdb07f98bdbba58cf17e4b33fa384a3
lavrton <lavrton@gmail.com>
2015-01-08 20:06:10 +0700

    editable duration. close #50

--- src/js/views/sideBar/TaskItem.js
+++ src/js/views/sideBar/TaskItem.js
@@ -34,36 +34,85 @@ var TaskItem = React.createClass({
         return this._createReadFiled(col);
     },
     _createReadFiled : function(col) {
+        var model = this.props.model;
         if (col === 'complete') {
-            return this.props.model.get(col) + '%';
+            return model.get(col) + '%';
         }
         if (col === 'start' || col === 'end') {
-            return this.props.model.get(col).toString('dd/MM/yyyy');
+            return model.get(col).toString('dd/MM/yyyy');
         }
-        return this.props.model.get(col);
+        if (col === 'duration') {
+            return Date.daysdiff(model.get('start'), model.get('end'))+' d';
+        }
+        return model.get(col);
     },
-    _createEditField : function(col) {
+    _createDateElement : function(col) {
         var val = this.props.model.get(col);
-        var el;
-        if (col === 'start' || col === 'end') {
-            el = DatePicker;
-        } else {
-            el = 'input';
-        }
-        return React.createElement(el, {
+        return React.createElement(DatePicker, {
             value : val,
             key : col,
             onChange : function(e) {
                 var newVal = e.target.value;
-                if (col === 'start' || col === 'end') {
+                var state = this.state;
+                state.editRow = undefined;
+                this.setState(state);
+                this.props.model.set(col, newVal);
+                this.props.model.save();
+            }.bind(this)
+        });
+    },
+    _durationChange : function(value) {
+        var number = parseInt(value.replace( /^\D+/g, ''), 10);
+        console.log(value.replace( /^\D+/g, ''), number);
+        if (!number) {
+            return;
+        }
+        if (value.indexOf('w') > -1) {
+            this.props.model.set('end', this.props.model.get('start').clone().addWeeks(number));
+        } else  if (value.indexOf('m') > -1) {
+            this.props.model.set('end', this.props.model.get('start').clone().addMonths(number));
+        } else {
+            number--;
+            this.props.model.set('end', this.props.model.get('start').clone().addDays(number));
+        }
+    },
+    _createDurationField : function() {
+        var val = Date.daysdiff(this.props.model.get('start'), this.props.model.get('end'))+' d';
+        return React.createElement('input', {
+            value : this.state.val || val,
+            key : 'duration',
+            onChange : function(e) {
+                var newVal = e.target.value;
+                this._durationChange(newVal);
+                var state = this.state;
+                state.val = newVal;
+                this.setState(state);
+            }.bind(this),
+            onKeyDown : function(e) {
+                if (e.keyCode === 13) {
                     var state = this.state;
                     state.editRow = undefined;
+                    state.val = undefined;
                     this.setState(state);
-                    this.props.model.set(col, newVal);
                     this.props.model.save();
-                } else {
-                    this.props.model.set(col, newVal);
                 }
+            }.bind(this)
+        });
+    },
+    _createEditField : function(col) {
+        var val = this.props.model.get(col);
+        if (col === 'start' || col === 'end') {
+            return this._createDateElement(col);
+        }
+        if (col === 'duration') {
+            return this._createDurationField();
+        }
+        return React.createElement('input', {
+            value : val,
+            key : col,
+            onChange : function(e) {
+                var newVal = e.target.value;
+                this.props.model.set(col, newVal);
             }.bind(this),
             onKeyDown : function(e) {
                 if (e.keyCode === 13) {
@@ -114,7 +163,7 @@ var TaskItem = React.createClass({
                 React.createElement('li', {
                     key : 'duration',
                     className : 'col-duration'
-                }, Date.daysdiff(model.get('start'),model.get('end'))+' d')
+                }, this._createField('duration'))
             );
     }
 });
