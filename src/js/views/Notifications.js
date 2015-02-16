c64b7215243a10852aadafbf351ee5f056a72f68
lavrton <lavrton@gmail.com>
2015-02-16 10:55:42 +0700

    show error notification. close #54

--- src/js/views/Notifications.js
+++ src/js/views/Notifications.js
@@ -1,11 +1,14 @@
 var Notifications = Backbone.View.extend({
     initialize : function() {
         this.listenTo(this.collection, 'error', this.onError);
-        this.listenTo(this.collection, 'success', this.onError);
     },
     onError : function() {
         console.error(arguments);
-        var n = noty({text: 'Error while saving task. Sorry...'});
+        noty({
+            text: 'Error while saving task. Sorry...',
+            layout : 'topRight',
+            type : 'error'
+        });
     }
 });
 
