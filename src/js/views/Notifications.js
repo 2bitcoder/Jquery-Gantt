1e7002d49054ad6cb2baf2afcd332c835e5bfd0a
lavrton <lavrton@gmail.com>
2015-02-19 06:52:58 +0700

    show only one error notification. Change notification text.

--- src/js/views/Notifications.js
+++ src/js/views/Notifications.js
@@ -1,11 +1,11 @@
 var Notifications = Backbone.View.extend({
     initialize : function() {
-        this.listenTo(this.collection, 'error', this.onError);
+        this.listenTo(this.collection, 'error', _.debounce(this.onError, 10));
     },
     onError : function() {
         console.error(arguments);
         noty({
-            text: 'Error while saving task. Sorry...',
+            text: 'Error while saving task, please refresh your browser, request support if this error continues.',
             layout : 'topRight',
             type : 'error'
         });
